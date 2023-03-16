33164dadd4d9e0818dd9e8e341dc7ac2e24cb111
jsalazarv <jsalazarv8@gmail.com>
2023-03-16 17:01:39 -0600

    layout (WIP)

---
+++ src/layouts/public.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="site-wrapper" id="top">
+    <Header />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
