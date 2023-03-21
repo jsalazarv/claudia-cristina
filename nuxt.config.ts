3d7b73bb47c5d78db7d975116c3506f1bb8e9861
jsalazarv <jsalazarv8@gmail.com>
2023-03-21 17:00:46 -0600

    Persist i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,32 @@
+import en from '~/locales/en';
+
 export default {
     buildModules: [
         '@nuxt/typescript-build',
         '@nuxtjs/axios',
     ],
+    modules: [
+        '@nuxtjs/i18n',
+    ],
+    i18n: {
+        locales: ['en', 'es'],
+        defaultLocale: 'en',
+        vueI18n: {
+            fallbackLocale: 'en',
+            messages: {
+                en: {
+                    header: {
+                        home: 'Home'
+                    }
+                },
+                es: {
+                    header: {
+                        home: 'Inicio'
+                    }
+                }
+            }
+        }
+    },
     axios: {
         baseURL: 'https://api.example.com',
     },
